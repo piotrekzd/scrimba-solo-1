d0ce80cd049e67938e1f6c140e1af0e05f74448b
piotrekzd <piotrek.zdebski@gmail.com>
2023-01-17 18:54:35 +0100

    Update Info.jsx

--- src/components/Info/Info.jsx
+++ src/components/Info/Info.jsx
@@ -4,8 +4,8 @@ export const Info = () => {
     return (
         <div className={styles.about_wrapper}>
             <img src='/src/images/avatar.png' className={styles.avatar} />
-            <h1 className={styles.title}>Laura Smith</h1>
-            <h2 className={styles.subtitle}>Frontend Developer</h2>
+            <h1 className={styles.title}>Piotr Zdebski</h1>
+            <h2 className={styles.subtitle}>Junior Frontend Developer</h2>
             <a href="_blank" className={styles.link}>laurasmith.website</a>
             <div className={styles.button_wrapper}>
                 <button type="button" className={styles.btn_mail}>Email</button>
